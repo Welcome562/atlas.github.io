bc4306b980e3860138c3755c170562f3d10002c6
proudparrot2 <hi@proudparrot2.tech>
2023-11-15 11:08:53 -0600

    i hate bare

--- public/uv/uv.config.js
+++ public/uv/uv.config.js
@@ -1,7 +1,7 @@
 /*global Ultraviolet*/
 self.__uv$config = {
     prefix: '/service/',
-    bare: 'https://tomp.app/',
+    bare: 'https://bare.worksheetsforteachers.org/',
     encodeUrl: Ultraviolet.codec.xor.encode,
     decodeUrl: Ultraviolet.codec.xor.decode,
     handler: '/uv/uv.handler.js',
