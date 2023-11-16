009e3cff5436498d6e2dfa925150e06a884006b1
proudparrot2 <hi@proudparrot2.tech>
2023-11-16 10:58:04 -0600

    remove reloader

--- src/index.ts
+++ src/index.ts
@@ -7,8 +7,3 @@ import './style.less'
 export const browser = new Browser()
 export const tabManager = new TabManager()
 export const frameManager = new FrameManager()
-
-if (!localStorage.getItem("loaded")) {
-  location.reload()
-  localStorage.setItem("loaded", "true")
-}
